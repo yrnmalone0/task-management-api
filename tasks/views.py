5116c4e5c000bcb02cf800894995ef525f89eab2
yrnmalone0 <obedbadu04@gmail.com>
2026-01-03 20:03:30 +0000

    feat: add endpoint to list all tasks

--- tasks/views.py
+++ tasks/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from .models import Task
 
 
 #register users
@@ -45,3 +46,18 @@ def create_task(request):
             status=status.HTTP_201_CREATED
         )
     return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+
+
+#view tasks
+@api_view(['GET'])
+def task_list(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+
+#view task details
+# @api_view(['GET'])
+# def task_details(request, pk):
+#     task_detail = Task.objects.get(id=pk)
+#     serializer = TaskSerializer
